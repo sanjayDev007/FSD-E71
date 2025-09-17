62240c3e1e32d088f3a0013739fb099d3cd51f28
Sanjay9745 <sanjysanthosh919@gmail.com>
2025-09-17 22:01:53 +0530

    new chnage

---
+++ react-part-1-sep-17/code/dom.js
@@ -0,0 +1,11 @@
+let document = {
+    html: {
+        head: {
+            title: { text: "Hello World" }
+        },
+        body: {
+            h1: { text: "Welcome to my website" },
+            p: { text: "This is a simple web page." }
+        }
+    }
+}
